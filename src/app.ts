0711ea4f13a5a41f71c75551a2b7c09bec055dfb
Adoff <zeddie365@gmail.com>
2026-04-08 16:26:07 +1000

    vid32

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,24 @@
 // const express = require("express");
 import express from "express";
+// import 'dotenv/config' - only TS
+require ('dotenv').config(); //require .env library - for js + ts
+
 const app = express();
-const port = 8080;
+const port = process.env.port || 8080;
 
 app.get("/", (req,res) =>{
-    res.send( "Hello World")
+    res.send( "Hello World Nodemon")
 })
 
 app.get("/abc", (req,res) =>{
     res.send( "Hello AE")
 })
 
+app.get("/html", (req,res) =>{
+    res.send(`<h1 style="color:red">Hello mấy ní</h1>`)
+})
+
 app.listen (port, () =>{
     console.log(`App is running on port: ${port}`)
+    console.log(`env.port: ${process.env.port}`)
 })
