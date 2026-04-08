40d5d0a76f6bec710b71e67d8f4dfd73876ed1bc
Adoff <zeddie365@gmail.com>
2026-04-08 15:08:51 +1000

    vid22

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 const express = require ('express');
 const app = express();
-const port = 5000;
+const PORT = 8080;
 
 app.get('/', (req,res) => {res.send('Hello World')});
 
-app.listen(port,()=> {
-    console.log(`Example app listening on port ${port}`)
+app.listen(PORT,()=> {
+    console.log(`Example app listening on port ${PORT}`)
 })
